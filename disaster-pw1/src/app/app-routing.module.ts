e23e93020c748d235e18724e0b6e5ceb9130453c
ananda guedes <anandaguedesdoo@gmail.com>
2025-02-10 18:02:03 -0300

    origin e volunteer

--- disaster-pw1/src/app/app-routing.module.ts
+++ disaster-pw1/src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ListagemComponent } from "./desastre/listagem/listagem.component";
 import { ManutencaoComponent } from "./desastre/manutencao/manutencao.component";
 
+import { OriginComponent } from './home/origin/origin.component';
+
 export const routes: Routes = [
     {
-    path: 'listagem-desastres',
-    component: ListagemComponent
+        path: 'listagem-desastres',
+        component: ListagemComponent
     },
     {
-    path: 'cadastro-desastre',
-    component: ManutencaoComponent
+        path: 'cadastro-desastre',
+        component: ManutencaoComponent
     },
     {
-    path: 'edicao-desastre/:id',
-    component: ManutencaoComponent
+        path: 'edicao-desastre/:id',
+        component: ManutencaoComponent
     },
-
+    {
+        path: 'app-origin',
+        component: OriginComponent
+    },
 ];
 
 @NgModule({
